d95de3326dafc01335c15b2481dc54fef23107b5
Aldo <75909548+kiriadix@users.noreply.github.com>
2023-09-28 18:59:18 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        
+        <TextInput placeholder="Enter your event"  style={styles.input} />
+        
+        <View style={styles.button}>
+          <Button title='Add' style={styles.button} color="#52528C"/>
+        </View>
+        
+      </View>
+      
     </View>
   );
 }
@@ -14,7 +21,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 20,
   },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  input: {
+    width: '80%',
+    marginRight: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#52528C',
+  },
+  button:{
+
+  }
 });
