c2b3ad2628208116e4fe69bea7e190503ae895c7
Aldo <75909548+kiriadix@users.noreply.github.com>
2023-10-06 11:04:04 -0300

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { styles } from "./styles.js";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Aldo eres el mejor</Text>
       <StatusBar style="auto" />
     </View>
   );
